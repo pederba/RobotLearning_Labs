a0383ed5d950f490e94dc1e9c738a12d8e6d9777
KristofferMoevik <kristoffersmovik@live.no>
2023-11-02 10:00:42 +0100

    init

---
+++ src/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/kristoffer/Documents/Skole/Robot learning/RobotLearning_Labs/src/catkin_ws/src
+# Build directory: /home/kristoffer/Documents/Skole/Robot learning/RobotLearning_Labs/src/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
---
+++ src/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/kristoffer/Documents/Skole/Robot learning/RobotLearning_Labs/src/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/kristoffer/Documents/Skole/Robot learning/RobotLearning_Labs/src/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/kristoffer/Documents/Skole/Robot learning/RobotLearning_Labs/src/catkin_ws/devel;/opt/ros/noetic' != '' else []
