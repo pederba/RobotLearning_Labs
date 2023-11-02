f45cb67459b5b33e745d7a2174108af366d0b384
Peder Raastad Basmo <peder.basmo@gmail.com>
2023-11-02 16:14:54 +0000

    add_two_ints_server funker ikke

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/workspaces/RobotLearning_Labs/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/workspaces/RobotLearning_Labs/devel;/opt/ros/noetic'.split(';') if '/workspaces/RobotLearning_Labs/devel;/opt/ros/noetic' != '' else []
---
+++ devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(beginner_tutorials_MESSAGE_FILES "/workspaces/RobotLearning_Labs/src/beginner_tutorials/msg/Num.msg")
+set(beginner_tutorials_SERVICE_FILES "/workspaces/RobotLearning_Labs/src/beginner_tutorials/srv/AddTwoInts.srv")
